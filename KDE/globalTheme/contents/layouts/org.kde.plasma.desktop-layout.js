eac265cd3d581fec659a263309e40bd6cde9e190
Mrcuve0 <puof@protonmail.com>
2021-04-21 19:41:28 +0200

    Fix Layout

--- KDE/globalTheme/contents/layouts/org.kde.plasma.desktop-layout.js
+++ KDE/globalTheme/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -28,7 +28,6 @@ var layout = {
                     "sortMode": "-1"
                 },
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "file:///home/sem/Pictures/Wallpapers/Aritim-Light_DEV/05.jpg",
                     "SlidePaths": "/usr/share/wallpapers"
                 }
             },
@@ -60,7 +59,6 @@ var layout = {
                     "sortMode": "-1"
                 },
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "file:///home/sem/Pictures/Wallpapers/Aritim-Light_DEV/05.jpg",
                     "SlidePaths": "/home/sem/.local/share/wallpapers,/usr/share/wallpapers"
                 }
             },
@@ -93,8 +91,6 @@ var layout = {
                     "sortMode": "-1"
                 },
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "/home/sem/Pictures/Firefly-Light/Official/Chrysalis_desktop_butterfly_bright_1920x1080.png",
-                    "SlidePaths": "/home/sem/.local/share/wallpapers,/usr/share/wallpapers"
                 }
             },
             "wallpaperPlugin": "org.kde.image"
@@ -299,11 +295,6 @@ var layout = {
                         "/Configuration/ConfigDialog": {
                             "DialogHeight": "540",
                             "DialogWidth": "720"
-                        },
-                        "/Configuration/General": {
-                            "Notification": "false",
-                            "servers": "[{\"status\":0,\"active\":true,\"hostname\":\"http://stream.lesonparisien.com/hi.mp3?.mp3&listening-from-radio-garden=1581197225514\",\"name\":\"Le Son Parisien - Paris\"},{\"status\":0,\"active\":true,\"hostname\":\"http://mixlive.ie:9608/;?listening-from-radio-garden=1581196715411\",\"name\":\"MixLive Progressive & Techno\"},{\"status\":0,\"active\":true,\"hostname\":\"https://radio.garden/api/ara/content/listen/ulNdnSFw/channel.mp3?1600529061528\",\"name\":\"Algorythme Drum & Bass - Paris\"},{\"status\":0,\"active\":true,\"hostname\":\"http://radio.garden/api/ara/content/listen/egq3oSRB/channel.mp3?1600529132721\",\"name\":\"M Radio French Touch - Paris\"},{\"status\":0,\"active\":false,\"hostname\":\"https://live.fem.c3voc.de/c3lounge_native.mp3\",\"name\":\"rC3 Lounge\"},{\"status\":0,\"active\":true,\"hostname\":\"http://icecast.unitedradio.it/Virgin.mp3\",\"name\":\"Virgin Radio Italia - Milano\"},{\"status\":0,\"active\":true,\"hostname\":\"http://stream15.top-ix.org/ondequadre\",\"name\":\"OndeQuadre - Torino\"},{\"status\":0,\"active\":true,\"hostname\":\"https://radio.garden/api/ara/content/listen/W1tk9e4e/channel.mp3?1600530175028\",\"name\":\"Prysm Nu Disco - Paris\"},{\"status\":0,\"active\":true,\"hostname\":\"http://radio.garden/api/ara/content/listen/JGVRc1hq/channel.mp3?1600529690580\",\"name\":\"Real Radio XS - Manchester\"},{\"status\":0,\"active\":true,\"hostname\":\"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p?listening-from-radio-garden=1581197007970\",\"name\":\"BBC Radio 1\"},{\"status\":0,\"active\":true,\"hostname\":\"http://radio.garden/api/ara/content/listen/yjqvv9lf/channel.mp3?1600528118573\",\"name\":\"BLAST.IT Radio - Torino\"},{\"status\":0,\"active\":true,\"hostname\":\"http://stream.radioblackout.org/blackout.mp3\",\"name\":\"Radio Blackout - Torino\"},{\"status\":0,\"active\":true,\"hostname\":\"https://radiodeejay-lh.akamaihd.net/i/RadioDeejay_Live_1@189857/master.m3u8\",\"name\":\"RadioDeeJay\"},{\"status\":0,\"active\":true,\"hostname\":\"http://radiom2o-lh.akamaihd.net/i/RadioM2o_Live_1@42518/master.m3u8\",\"name\":\"Radio M2O\"},{\"status\":0,\"active\":true,\"hostname\":\"https://radio.garden/api/ara/content/listen/K4tCXS0i/channel.mp3?1600528778637\",\"name\":\"NRJ - Chill - Paris\"},{\"status\":0,\"active\":true,\"hostname\":\"http://icestreaming.rai.it/1.mp3\",\"name\":\"Rai Radio 1\"},{\"status\":0,\"active\":true,\"hostname\":\"http://icestreaming.rai.it/2.mp3\",\"name\":\"Rai Radio 2\"},{\"status\":0,\"active\":true,\"hostname\":\"http://icestreaming.rai.it/3.mp3\",\"name\":\"Rai Radio 3\"},{\"status\":0,\"active\":true,\"hostname\":\"http://shoutcast.radio24.it:8000/\",\"name\":\"Radio 24\"},{\"status\":0,\"active\":true,\"hostname\":\"http://shoutcast.rtl.it:3060/\",\"name\":\"RadioFreccia\"},{\"status\":0,\"active\":true,\"hostname\":\"http://radio.garden/api/ara/content/listen/GMuGZylB/channel.mp3?1600529263070\",\"name\":\"Pigalle Paris Radio - Paris\"},{\"status\":0,\"active\":true,\"hostname\":\"http://radio.garden/api/ara/content/listen/eiclmnvi/channel.mp3?1600529434624\",\"name\":\"Jazz Radio Lounge - Lyon\"},{\"status\":0,\"active\":true,\"hostname\":\"https://radio.garden/api/ara/content/listen/Kk1xtdz5/channel.mp3?1600529928985\",\"name\":\"House Music Radio - London\"},{\"status\":0,\"active\":true,\"hostname\":\"https://radio.garden/api/ara/content/listen/aqL36uFi/channel.mp3?1600530450628\",\"name\":\"PaulinRio Radio - Rio de Janeiro\"}]",
-                            "version": "1.8"
                         }
                     },
                     "plugin": "org.kde.plasma.advancedradio"
@@ -355,20 +346,6 @@ var layout = {
                             "clockTimeFormat2": "yyyy-MM-dd",
                             "v71Migration": "true",
                             "v72Migration": "true"
-                        },
-                        "/Configuration/Google%20Calendar": {
-                            "accessToken": "ya29.a0AfH6SMAeEcpuPYfVj_BN0WeHK5csxLyh4DIfn199HPKo1QLGH-9HaTEyEAol30OjUqpbyGOooVLI7Z4MKHnvQxxqILlaebHtltznID05yXLHOJIp9sm7hzbc_RIly3Y_zNqQKK6jGjX5h5qvI9mov261Szcmmw",
-                            "accessTokenExpiresAt": "4118644359",
-                            "accessTokenType": "Bearer",
-                            "calendarIdList": "addressbook#contacts@group.v.calendar.google.com,it.italian#holiday@group.v.calendar.google.com,giabertini93@gmail.com,vkjs1orpvhu710hfg8tqa6sj8spnsgua@import.calendar.google.com",
-                            "calendarList": "W3sia2luZCI6ImNhbGVuZGFyI2NhbGVuZGFyTGlzdEVudHJ5IiwiZXRhZyI6IlwiMTU4Mzc3NDkyMzc3MzAwMFwiIiwiaWQiOiJ0dHRuNzZqc2YyamZjbGoxc2FobHBnMWRjOWpvMDRxcUBpbXBvcnQuY2FsZW5kYXIuZ29vZ2xlLmNvbSIsInN1bW1hcnkiOiJodHRwOi8vd3d3LnN3YXMucG9saXRvLml0L2RvdG5ldC9vcmFyaV9sZXppb25lX3B1Yi9FeHBvcnRfaWNzLmFzcHg/aWRfaW10PTE2NDY1NyZudW1jb3I9MCZhX2FjYz0yMDIwJnBkPTEiLCJ0aW1lWm9uZSI6IkV1cm9wZS9Sb21lIiwic3VtbWFyeU92ZXJyaWRlIjoiRW5lcmd5IE1hbmdlbWVudCBmb3IgSW9UIiwiY29sb3JJZCI6IjEzIiwiYmFja2dyb3VuZENvbG9yIjoiIzkyZTFjMCIsImZvcmVncm91bmRDb2xvciI6IiMwMDAwMDAiLCJhY2Nlc3NSb2xlIjoicmVhZGVyIiwiZGVmYXVsdFJlbWluZGVycyI6W10sImNvbmZlcmVuY2VQcm9wZXJ0aWVzIjp7ImFsbG93ZWRDb25mZXJlbmNlU29sdXRpb25UeXBlcyI6WyJoYW5nb3V0c01lZXQiXX19LHsia2luZCI6ImNhbGVuZGFyI2NhbGVuZGFyTGlzdEVudHJ5IiwiZXRhZyI6IlwiMTYwNjA0NTY2MjcyMzAwMFwiIiwiaWQiOiJpdC5pdGFsaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29tIiwic3VtbWFyeSI6IkZlc3Rpdml0w6AgaW4gSXRhbGlhIiwiZGVzY3JpcHRpb24iOiJGZXN0aXZpdMOgIGUgcmljb3JyZW56ZSBpbiBJdGFsaWEiLCJ0aW1lWm9uZSI6IkV1cm9wZS9Sb21lIiwic3VtbWFyeU92ZXJyaWRlIjoiRmVzdGl2aXTDoCBpbiBJdGFsaWEiLCJjb2xvcklkIjoiOCIsImJhY2tncm91bmRDb2xvciI6IiMxNmE3NjUiLCJmb3JlZ3JvdW5kQ29sb3IiOiIjMDAwMDAwIiwic2VsZWN0ZWQiOnRydWUsImFjY2Vzc1JvbGUiOiJyZWFkZXIiLCJkZWZhdWx0UmVtaW5kZXJzIjpbXSwiY29uZmVyZW5jZVByb3BlcnRpZXMiOnsiYWxsb3dlZENvbmZlcmVuY2VTb2x1dGlvblR5cGVzIjpbImhhbmdvdXRzTWVldCJdfX0seyJraW5kIjoiY2FsZW5kYXIjY2FsZW5kYXJMaXN0RW50cnkiLCJldGFnIjoiXCIxNjA2MDQ1Njc4Njc0MDAwXCIiLCJpZCI6InZranMxb3Jwdmh1NzEwaGZnOHRxYTZzajhzcG5zZ3VhQGltcG9ydC5jYWxlbmRhci5nb29nbGUuY29tIiwic3VtbWFyeSI6Imh0dHBzOi8vZjFjYWxlbmRhci5jb20vZG93bmxvYWQvZjEtY2FsZW5kYXJfcDFfcDJfcDNfcV9ncF9hbGFybS00MC5pY3M/dD0xNTgzOTIwODc2MTIyIiwidGltZVpvbmUiOiJVVEMiLCJzdW1tYXJ5T3ZlcnJpZGUiOiJGb3JtdWxhIDEiLCJjb2xvcklkIjoiMyIsImJhY2tncm91bmRDb2xvciI6IiNmODNhMjIiLCJmb3JlZ3JvdW5kQ29sb3IiOiIjMDAwMDAwIiwic2VsZWN0ZWQiOnRydWUsImFjY2Vzc1JvbGUiOiJyZWFkZXIiLCJkZWZhdWx0UmVtaW5kZXJzIjpbXSwiY29uZmVyZW5jZVByb3BlcnRpZXMiOnsiYWxsb3dlZENvbmZlcmVuY2VTb2x1dGlvblR5cGVzIjpbImhhbmdvdXRzTWVldCJdfX0seyJraW5kIjoiY2FsZW5kYXIjY2FsZW5kYXJMaXN0RW50cnkiLCJldGFnIjoiXCIxNjA4OTcwNjc5NDE0MDAwXCIiLCJpZCI6Ijgzczd0MGUycWZsZXFudDJjcWh0NmxudTZ1aTNhYXBrQGltcG9ydC5jYWxlbmRhci5nb29nbGUuY29tIiwic3VtbWFyeSI6Imh0dHA6Ly93d3cuc3dhcy5wb2xpdG8uaXQvZG90bmV0L29yYXJpX2xlemlvbmVfcHViL0V4cG9ydF9pY3MuYXNweD9pZF9pbXQ9MTU4MjYwJm51bWNvcj0wJmFfYWNjPTIwMjAmcGQ9MiIsInRpbWVab25lIjoiRXVyb3BlL1JvbWUiLCJzdW1tYXJ5T3ZlcnJpZGUiOiJNUyIsImNvbG9ySWQiOiI0IiwiYmFja2dyb3VuZENvbG9yIjoiI2ZhNTczYyIsImZvcmVncm91bmRDb2xvciI6IiMwMDAwMDAiLCJhY2Nlc3NSb2xlIjoicmVhZGVyIiwiZGVmYXVsdFJlbWluZGVycyI6W10sImNvbmZlcmVuY2VQcm9wZXJ0aWVzIjp7ImFsbG93ZWRDb25mZXJlbmNlU29sdXRpb25UeXBlcyI6WyJoYW5nb3V0c01lZXQiXX19LHsia2luZCI6ImNhbGVuZGFyI2NhbGVuZGFyTGlzdEVudHJ5IiwiZXRhZyI6IlwiMTYwODk3MDY4MjI2NDAwMFwiIiwiaWQiOiJraHJrcDRhc2tmMW01NGdlaDJwbnRwMHMyb2lkaXIyaEBpbXBvcnQuY2FsZW5kYXIuZ29vZ2xlLmNvbSIsInN1bW1hcnkiOiJodHRwOi8vd3d3LnN3YXMucG9saXRvLml0L2RvdG5ldC9vcmFyaV9sZXppb25lX3B1Yi9FeHBvcnRfaWNzLmFzcHg/aWRfaW10PTE2NDY1NiZudW1jb3I9MCZhX2FjYz0yMDIwJnBkPTIiLCJ0aW1lWm9uZSI6IkV1cm9wZS9Sb21lIiwic3VtbWFyeU92ZXJyaWRlIjoiQ3liZXJzZWN1cml0eSIsImNvbG9ySWQiOiIxNSIsImJhY2tncm91bmRDb2xvciI6IiM5ZmM2ZTciLCJmb3JlZ3JvdW5kQ29sb3IiOiIjMDAwMDAwIiwiYWNjZXNzUm9sZSI6InJlYWRlciIsImRlZmF1bHRSZW1pbmRlcnMiOltdLCJjb25mZXJlbmNlUHJvcGVydGllcyI6eyJhbGxvd2VkQ29uZmVyZW5jZVNvbHV0aW9uVHlwZXMiOlsiaGFuZ291dHNNZWV0Il19fSx7ImtpbmQiOiJjYWxlbmRhciNjYWxlbmRhckxpc3RFbnRyeSIsImV0YWciOiJcIjE2MTUwNzIzMzk5NTgwMDBcIiIsImlkIjoiYjQzNTJxZjhrMjI3YmFiOHRqZHVwNjFhOGIxYTNkZ2xAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20iLCJzdW1tYXJ5IjoiaHR0cHM6Ly93d3cudHJhaW5saW5lLmV1L2NhbGVuZGFycy94aHh1dW5ycXBwZWJrc2RiZ3lrYXhnZXFiYWZodWtjY2xiLmljcyIsInRpbWVab25lIjoiVVRDIiwic3VtbWFyeU92ZXJyaWRlIjoiVHJhaW5MaW5lIiwiY29sb3JJZCI6IjEzIiwiYmFja2dyb3VuZENvbG9yIjoiIzkyZTFjMCIsImZvcmVncm91bmRDb2xvciI6IiMwMDAwMDAiLCJhY2Nlc3NSb2xlIjoicmVhZGVyIiwiZGVmYXVsdFJlbWluZGVycyI6W10sImNvbmZlcmVuY2VQcm9wZXJ0aWVzIjp7ImFsbG93ZWRDb25mZXJlbmNlU29sdXRpb25UeXBlcyI6WyJoYW5nb3V0c01lZXQiXX19LHsia2luZCI6ImNhbGVuZGFyI2NhbGVuZGFyTGlzdEVudHJ5IiwiZXRhZyI6IlwiMTYxNTA3MjM0MDYwODAwMFwiIiwiaWQiOiJhZGRyZXNzYm9vayNjb250YWN0c0Bncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20iLCJzdW1tYXJ5IjoiQ29tcGxlYW5uaSIsImRlc2NyaXB0aW9uIjoiTW9zdHJhIGkgY29tcGxlYW5uaSwgZ2xpIGFubml2ZXJzYXJpIGUgbGUgZGF0ZSBkaSBhbHRyaSBldmVudGkgZGVpIHR1b2kgQ29udGF0dGkgR29vZ2xlLiIsInRpbWVab25lIjoiRXVyb3BlL1JvbWUiLCJzdW1tYXJ5T3ZlcnJpZGUiOiJCaXJ0aGRheXMiLCJjb2xvcklkIjoiMTMiLCJiYWNrZ3JvdW5kQ29sb3IiOiIjOTJlMWMwIiwiZm9yZWdyb3VuZENvbG9yIjoiIzAwMDAwMCIsImFjY2Vzc1JvbGUiOiJyZWFkZXIiLCJkZWZhdWx0UmVtaW5kZXJzIjpbXSwiY29uZmVyZW5jZVByb3BlcnRpZXMiOnsiYWxsb3dlZENvbmZlcmVuY2VTb2x1dGlvblR5cGVzIjpbImhhbmdvdXRzTWVldCJdfX0seyJraW5kIjoiY2FsZW5kYXIjY2FsZW5kYXJMaXN0RW50cnkiLCJldGFnIjoiXCIxNjE1MDcyMzQxNzA5MDAwXCIiLCJpZCI6ImdpYWJlcnRpbmk5M0BnbWFpbC5jb20iLCJzdW1tYXJ5IjoiZ2lhYmVydGluaTkzQGdtYWlsLmNvbSIsInRpbWVab25lIjoiRXVyb3BlL1JvbWUiLCJjb2xvcklkIjoiMTQiLCJiYWNrZ3JvdW5kQ29sb3IiOiIjOWZlMWU3IiwiZm9yZWdyb3VuZENvbG9yIjoiIzAwMDAwMCIsInNlbGVjdGVkIjp0cnVlLCJhY2Nlc3NSb2xlIjoib3duZXIiLCJkZWZhdWx0UmVtaW5kZXJzIjpbeyJtZXRob2QiOiJwb3B1cCIsIm1pbnV0ZXMiOjMwfV0sIm5vdGlmaWNhdGlvblNldHRpbmdzIjp7Im5vdGlmaWNhdGlvbnMiOlt7InR5cGUiOiJldmVudENyZWF0aW9uIiwibWV0aG9kIjoiZW1haWwifSx7InR5cGUiOiJldmVudENoYW5nZSIsIm1ldGhvZCI6ImVtYWlsIn0seyJ0eXBlIjoiZXZlbnRDYW5jZWxsYXRpb24iLCJtZXRob2QiOiJlbWFpbCJ9LHsidHlwZSI6ImV2ZW50UmVzcG9uc2UiLCJtZXRob2QiOiJlbWFpbCJ9XX0sInByaW1hcnkiOnRydWUsImNvbmZlcmVuY2VQcm9wZXJ0aWVzIjp7ImFsbG93ZWRDb25mZXJlbmNlU29sdXRpb25UeXBlcyI6WyJoYW5nb3V0c01lZXQiXX19XQ==",
-                            "refreshToken": "1//093I7fBoR3YcgCgYIARAAGAkSNwF-L9IrCAy1Kpgdckrj_R8ZMmZkFBdW6z87_wszgONUWCarQd0YaF3-n6Y6B8vOoKXmh2R1_8A",
-                            "sessionClientId": "391436299960-k0s16nm589meovhoblpcquqgbbrena17.apps.googleusercontent.com",
-                            "sessionClientSecret": "Gdr_7lKIQuGBD4Up3MOiw-7i",
-                            "tasklistList": "W3sia2luZCI6InRhc2tzI3Rhc2tMaXN0IiwiaWQiOiJNREkzT1RRNU5UTTRNVGcyTVRRM016azRPVEU2TURvdyIsImV0YWciOiJcIk1UazVOVGd6TlRjek9BXCIiLCJ0aXRsZSI6Ik15IFRhc2tzIiwidXBkYXRlZCI6IjIwMTktMDYtMjBUMjI6MTY6MDMuOTIwWiIsInNlbGZMaW5rIjoiaHR0cHM6Ly93d3cuZ29vZ2xlYXBpcy5jb20vdGFza3MvdjEvdXNlcnMvQG1lL2xpc3RzL01ESTNPVFE1TlRNNE1UZzJNVFEzTXprNE9URTZNRG93In1d"
-                        },
-                        "/Configuration/Weather": {
-                            "openWeatherMapCityId": "3182884"
                         }
                     },
                     "plugin": "org.kde.plasma.eventcalendar"
@@ -540,6 +517,6 @@ var layout = {
     ],
     "serializationFormatVersion": "1"
 }
-;
+    ;
 
 plasma.loadSerializedLayout(layout);
